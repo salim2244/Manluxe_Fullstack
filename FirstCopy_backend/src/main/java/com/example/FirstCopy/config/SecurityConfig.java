54b955b76b64e1f97685a70a814bfb8a356baf3f
salim2244 <salimansari8308@gmail.com>
2026-07-31 19:31:22 +0530

    add webconfig file

--- FirstCopy_backend/src/main/java/com/example/FirstCopy/config/SecurityConfig.java
+++ FirstCopy_backend/src/main/java/com/example/FirstCopy/config/SecurityConfig.java
@@ -87,7 +87,10 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:4200",
+                "https://manluxe-fullstack.vercel.app"
+        ));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
